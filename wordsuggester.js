bc74ef8551e5a5e94d267430bc759bb8801fed83
emilyrn18 <emilyrn18@vt.edu>
2022-03-05 17:56:50 -0500

    Created word suggester script with TODOS

---
+++ wordsuggester.js
@@ -0,0 +1,11 @@
+var dictionary = require("./parseddictionary.txt");
+
+// TODO: Import or implement letter frequency in English language
+
+// TODO: Include user input of letters already used
+
+// TODO: Include letters that are guaranteed to be in word and the location the letter should be in if provided
+
+// TODO: Include user input of words already tried and how many tries user has left
+
+// TODO: Produce up to top 10 word suggestions based on above criteria
