83f9d5f5513575a2fe2f3f040b079b803c666cd4
emilyrn18 <emilyrn18@vt.edu>
2022-03-05 17:43:10 -0500

    documented and changed script name

--- generatedictionary.js
+++ generatedictionary.js
@@ -2,12 +2,15 @@ var EnglishWords = require('english-words')
 var usedwordlewords = require("./usedworddictionary.json");
 const fs = require('fs');
 
-
+//This determines to the word is within an array
 function arrayContains(thearray, wordtofind)
 {
     return (thearray.indexOf(wordtofind) > -1);
 }
 
+//Removes all words that have already been used in wordle
+//from the english dictionary array and it returns
+//a no array
 function removewords(englisharray){
     var arr = englisharray;
     for (var year of Object.keys(usedwordlewords)) {
@@ -22,6 +25,9 @@ function removewords(englisharray){
     return arr;
 }
 
+//This function creates the new dictionary by pulling out
+//all 5 letter words and removing already used words.
+//Then it outputs a text file of the new dictionary
 function createdictionary(){
     EnglishWords.getWords(
         function(words){
